59507b71ba9842a6fd70dbca81cd92e7376f789f
David Chen <davidc3287@gmail.com>
2021-02-06 10:59:48 -0500

    add comments

--- image_resizer.py
+++ image_resizer.py
@@ -16,7 +16,7 @@ resized_path = Path('./assets/resized')
 new_size = (64, 64)
 
 # shutil.rmtree(resized_path)  # clear previous files
-os.mkdir(resized_path)
+os.mkdir(resized_path)  # re-add resized directory
 for img_path in orig_path.iterdir():
     img = Image.open(img_path)
     img_resized = img.resize(new_size, Image.ANTIALIAS)
