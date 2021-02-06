06faf5c8bf93372c36d71f27d52ba4a4fae09c8d
David Chen <davidc3287@gmail.com>
2021-02-06 11:52:39 -0500

    update paths

--- pingmote.py
+++ pingmote.py
@@ -3,20 +3,18 @@ A Python GUI for selecting and inserting local images
 
 Author: David Chen
 '''
-
 import PySimpleGUI as sg
 import subprocess
 from pathlib import Path
 
 # CONFIGS
 GUI_BG_COLOR = '#36393F'  # copied from discord colors
-WINDOW_LOCATION = (1250, 750)
+WINDOW_LOCATION = (1000, 750)
 NUM_COLS = 10  # max number of images per row in picker
 
 
 def copy_to_clipboard(img_path):
     """ Given an an image path, copy the image to clipboard """
-    # command = f'xclip -sel clip -t image/png {img_path.absolute()}'
     command = f'xclip -sel clip -t image/png {img_path.absolute()}'
     subprocess.run(command.split())
 
@@ -26,12 +24,12 @@ sg.theme('LightBrown1')  # Use this as base theme
 sg.SetOptions(button_color=(GUI_BG_COLOR, GUI_BG_COLOR), background_color=GUI_BG_COLOR,
               text_element_background_color=GUI_BG_COLOR, text_color='white', border_width=0, window_location=WINDOW_LOCATION)
 
-image_path = Path('.') / 'assets' / 'resized'
+image_path = Path('/home/dchen327/coding/projects/pingmote/assets/resized')
 
 # layout the window
 layout = []
 curr_row = []
-print(len(list(image_path.iterdir())))
+# print(len(list(image_path.iterdir())))  # print number of images
 for idx, img in enumerate(image_path.iterdir(), start=1):  # add images to layout
     curr_row.append(
         sg.Button('', key=img, image_filename=img, image_subsample=1))
@@ -47,6 +45,5 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:  # X clicked
         break
+    window.close()
     copy_to_clipboard(event)  # copy clicked image to clipboard
-
-window.close()
