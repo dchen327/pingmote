9150f4bf9a33931a82192fb190a7938246e7883d
David Chen <davidc3287@gmail.com>
2021-02-07 22:46:58 -0500

    minor updates

--- pingmote.py
+++ pingmote.py
@@ -18,8 +18,7 @@ NUM_COLS = 12  # max number of images per row in picker
 NUM_FREQUENT = 12  # max number of images to show in the frequent section
 # absolute paths necessary here if running the program globally
 MAIN_PATH = Path('/home/dchen327/coding/projects/pingmote/')
-IMAGE_PATH = Path('/home/dchen327/coding/projects/pingmote/assets/resized')
-# IMAGE_PATH = Path('.') / 'assets/resized'
+IMAGE_PATH = MAIN_PATH / 'assets' / 'resized'
 AUTO_PASTE = False  # if True, automatically pastes the image after selection
 # if True and AUTO_PASTE is True, hits enter after pasting (useful in Discord)
 AUTO_ENTER = False
