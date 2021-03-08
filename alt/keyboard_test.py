d68502cb5337c4670095c59d89e611e3d041d993
David Chen <davidc3287@gmail.com>
2021-03-08 18:09:54 -0500

    further testing for hotkeys being swallowed

--- alt/keyboard_test.py
+++ alt/keyboard_test.py
@@ -1,5 +1,21 @@
 import keyboard
 
-keyboard.add_hotkey('windows+a', print, args=('triggered', 'hotkey'))
+hotkeys = {
+    "alt+w": "explorer.exe",
+}
 
+
+def customHotkey(event):
+    for hotkeys__key, hotkeys__value in hotkeys.items():
+        pressed = all(
+            keyboard.is_pressed(split__value) != False
+            for split__value in hotkeys__key.split('+')
+        )
+
+        if pressed:
+            # do some actions
+            print(pressed, hotkeys__value)
+
+
+keyboard.hook(customHotkey)
 keyboard.wait('esc')
