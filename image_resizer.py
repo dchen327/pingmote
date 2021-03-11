eb604d99f5f9b04ea43eaa6b2e22c89b6024fd36
David Chen <davidc3287@gmail.com>
2021-03-10 23:45:10 -0500

    remove some emotes

--- image_resizer.py
+++ image_resizer.py
@@ -8,11 +8,13 @@ Author: David Chen
 '''
 import shutil
 import os
+import json
 from PIL import Image
 from pathlib import Path
 
-orig_path = Path('./assets/original')
-resized_path = Path('./assets/resized')
+asset_path = Path(__file__).parent / 'assets'
+orig_path = asset_path / 'original'
+resized_path = asset_path / 'resized'
 new_size = (64, 64)
 
 
@@ -22,6 +24,28 @@ def sanitize_name(name):
     for c in replace_chars:
         name = name.replace(c, '')  # remove these chars
     return name.lower()
+
+
+def load_frequencies():
+    """ Load the frequencies dictionary from frequencies.json """
+    with open(asset_path / 'frequencies.json', 'r') as f:
+        return json.load(f)
+
+
+def write_frequencies(frequencies):
+    """ Write new frequencies to frequencies.json """
+    with open(asset_path / 'frequencies.json', 'w') as f:
+        json.dump(frequencies, f, indent=4)
+
+
+def clean_frequencies():
+    """ Clean frequencies.json on file changes """
+    frequencies = load_frequencies()
+    filenames = {img_path.name for img_path in resized_path.iterdir()}
+    for file in list(frequencies):
+        if file not in filenames:
+            del frequencies[file]  # remove key, file not present
+    write_frequencies(frequencies)
 
 
 shutil.rmtree(resized_path)  # clear previous files
@@ -35,3 +59,5 @@ for img_path in orig_path.iterdir():
         img = Image.open(img_path)
         img_resized = img.resize(new_size, Image.ANTIALIAS)
         img_resized.save(save_path)
+
+clean_frequencies()
