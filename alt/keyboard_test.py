38f48f84ca5e907b5372973a28e5d2ac0544145c
David Chen <davidc3287@gmail.com>
2021-03-13 12:56:25 -0500

    Revert "Feature/platform specific"

--- alt/keyboard_test.py
+++ alt/keyboard_test.py
@@ -1,20 +1,31 @@
-"""
-Prints the scan code of all currently pressed keys.
-Updates on every keyboard event.
-"""
 import keyboard
-import sys
-sys.path.append('..')
+import chime
 
-SHORTCUT = 'ctrl+q'
-
-
-def print_pressed_keys(e):
-    line = ', '.join(str(code) for code in keyboard._pressed_events)
-    # '\r' and end='' overwrites the previous line.
-    # ' '*40 prints 40 spaces at the end to ensure the previous line is cleared.
-    print('\r' + line + ' '*40, end='')
+print('hi from kb test')
 
 
-keyboard.hook(print_pressed_keys)
-keyboard.wait()
+def customHotkey(event):
+    for hotkey, func in hotkeys.items():
+        pressed = all(
+            keyboard.is_pressed(split__value) != False
+            for split__value in hotkey.split('+')
+        )
+
+        if pressed:
+            # do some actions
+            print(pressed, func)
+            func()
+            chime.success()
+
+
+def print_stuff():
+    print('print stuff')
+
+
+hotkeys = {
+    "alt+w": print_stuff
+}
+
+
+keyboard.hook(customHotkey)
+keyboard.wait('esc')
