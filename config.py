fa08af2c69d26897b70fc90f69bb4f3f0b6f20a0
David Chen <davidc3287@gmail.com>
2021-03-13 21:48:19 -0500

    clearer configs

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
+# sourcery skip: assign-if-exp
 from pathlib import Path
 import platform
 
 """ Hotkeys """
-SHORTCUT = 'ctrl+q' if platform.system() != 'Darwin' else 'command+3'
-KILL_SHORTCUT = 'alt+shift+k' if platform.system() != 'Darwin' else 'command+4'
+if platform.system() != 'Darwin':  # Windows and Linux
+    SHORTCUT = 'ctrl+q'
+    KILL_SHORTCUT = 'alt+shift+k'
+else:  # Mac
+    SHORTCUT = 'command+3'
+    KILL_SHORTCUT = 'command+4'
 
 """ Emote Picker """
 NUM_COLS = 12  # max number of images per row in picker
-SHOW_FREQUENTS = True  # show the frequents section at the top
 NUM_FREQUENT = 12  # max number of images to show in the frequent section
 SHOW_LABELS = True  # show section labels (frequents, static, gifs)
 SEPARATE_GIFS = True  # separate static emojis and gifs into different sections
@@ -24,6 +28,5 @@ IMAGE_PATH = MAIN_PATH / 'assets' / 'resized'  # resized emotes
 
 """ Experimental """
 SLEEP_TIME = 0  # add delay if pasting/enter not working
-# paste by inserting rather than copy paste, can be unreliable
-PRESERVE_CLIPBOARD = False
+PRESERVE_CLIPBOARD = False  # avoids copying link to clipboard (unreliable)
 CUSTOM_HOTKEY_HANDLER = False  # workaround for alt+tab issues
